3c2bfff9e16b174731825f0cac79087e1957cec0
Hayk Aleksanyan <aleksanianhayk@gmail.com>
2025-07-27 20:50:55 +0400

    course completion

--- frontend/src/components/CourseCompletion.jsx
+++ frontend/src/components/CourseCompletion.jsx
@@ -14,13 +14,13 @@ const CourseCompletion = ({ courseTitle }) => {
             <Confetti
                 width={width}
                 height={height}
-                numberOfPieces={6000} // A lot of confetti for a big celebration
+                numberOfPieces={8000} // A lot of confetti for a big celebration
                 recycle={false}
-                gravity={0.2}      // Makes confetti fall slower
-                wind={0}     // A gentle breeze
-                initialVelocityX={{ min: -10, max: 10 }} // Spread out horizontally
+                gravity={0.1}      // Makes confetti fall slower
+                wind={0.05}     // A gentle breeze
+                initialVelocityX={{ min: -100, max: 100 }} // Spread out horizontally
                 initialVelocityY={{ min: -15, max: 5 }}  // Shoot upwards initially
-                tweenDuration={9000} // Linger for a long time (15 seconds)
+                tweenDuration={10000} // Linger for a long time (15 seconds)
                 colors={['#78C841', '#B4E50D', '#FF9B2F', '#FB4141', '#FFFFFF']}
             />
             
